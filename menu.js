7e3cb5b62ec0ace252900159bb9220d071ab7f9a
Antti Saaristo <antti.saaristo@gmail.com>
2021-02-28 23:13:20 +0200

    share menu between pages and changed menu image

---
+++ menu.js
@@ -0,0 +1,2 @@
+document.write("<li class=\"active\"><a href=\"index.html\"><span><small>1&nbsp;<\/small>Home<\/span><\/a><\/li>");
+document.write("                <li><a href=\"https:\/\/saaristoantti.github.io\/robot\"><span><small>2<\/small>Robot Framework<\/span><\/a><\/li>");

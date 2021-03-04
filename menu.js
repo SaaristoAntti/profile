a5f74420a105f1ef8efb514bf42b8c067b54f8ca
Antti Saaristo <antti.saaristo@gmail.com>
2021-03-04 21:31:14 +0200

    added resume (about.hmtl) page

--- menu.js
+++ menu.js
@@ -1,2 +1,3 @@
 document.write("<li class=\"active\"><a href=\"index.html\"><span><small>1&nbsp;<\/small>Home<\/span><\/a><\/li>");
-document.write("                <li><a href=\"https:\/\/saaristoantti.github.io\/robot\"><span><small>2<\/small>Robot Framework<\/span><\/a><\/li>");
+document.write("<li><a href=\"about.html\"><span><small>2&nbsp;<\/small>Resume<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/saaristoantti.github.io\/robot\"><span><small>3<\/small>Robot Framework<\/span><\/a><\/li>");
